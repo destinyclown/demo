613aab090e5194d344c6e2cc941481c45361bc5b
Android <1842119037@qq.com>
2019-08-30 16:27:51 +0800

    Initial commit

---
+++ src/main/java/com/didispace/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.didispace.mapper;
+
+import com.didispace.entity.User;
+
+
+
+public interface UserMapper {
+ User Sel(int id);
+}
---
+++ src/main/java/com/didispace/service/UserService.java
@@ -0,0 +1,15 @@
+package com.didispace.service;
+
+import com.didispace.entity.User;
+import com.didispace.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserMapper userMapper;
+    public User Sel(int id){
+        return userMapper.Sel(id);
+    }
+}
